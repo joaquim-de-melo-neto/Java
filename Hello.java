57309ae0a838a57ca739ea6a263ea6b859b730fd
miershooptier0 <113483260+miershooptier0@users.noreply.github.com>
2023-01-09 19:11:42 -0300

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,7 @@
+//* Primeiro program Hello World*/
+
+public class Hello{
+	public static void main(String[] args){ // Início do método main
+		System.out.println("         Hello \rWorld");
+	} // Fim do método main
+}
---
+++ Soma.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class Soma{
+
+public static void main(String[] args){
+
+	int n1, n2, sum;
+
+	n1 = Integer.parseInt(JOptionPane.showInputDialog("Informe o primeiro número"));
+	n2 = Integer.parseInt(JOptionPane.showInputDialog("Informe o segundo número"));
+
+	sum = n1 + n2;
+
+	String message = String.format("A soma é: %d",sum);
+	
+	JOptionPane.showMessageDialog(null,"A soma é: "+sum);
+}
+}
