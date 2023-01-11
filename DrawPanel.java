08e4812550dd55a0a82d2eb93e8bd6bcbf188018
miershooptier0 <113483260+miershooptier0@users.noreply.github.com>
2023-01-11 18:43:20 -0300

    Add files via upload
    Desenha um painel com um X.

---
+++ DrawPanel.java
@@ -0,0 +1,14 @@
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class DrawPanel extends JPanel{
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+
+		int width = getWidth();
+		int heigth = getHeight();
+
+		g.drawLine(0,0,width,heigth);
+		g.drawLine(0,heigth,width,0);
+	}
+}
