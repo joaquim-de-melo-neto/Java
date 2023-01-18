0ccd4717dfe517a9b3fe81a9c71376da070af64d
miershooptier0 <113483260+miershooptier0@users.noreply.github.com>
2023-01-18 20:11:12 -0300

    Add files via upload
    Exemplos de métodos que copiam uma array para outra.

---
+++ ArrayCopyDemo.java
@@ -0,0 +1,30 @@
+// Copia os elementos de uma array para outra.
+class ArrayCopyDemo {
+    public static void main(String[] args) {
+        String[] copyFrom = {
+            "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",   
+            "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",      
+            "Marocchino", "Ristretto" };
+        
+        String[] copyTo = new String[7];
+        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
+        for (String coffee : copyTo) {
+            System.out.print(coffee + " ");           
+        }
+    }
+}
+
+// Diferentemente do primeiro esse não precisa do array de destino
+class ArrayCopyOfDemo {
+    public static void main(String[] args) {
+        String[] copyFrom = {
+            "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",   
+            "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",      
+            "Marocchino", "Ristretto" };
+        
+        String[] copyTo = java.util.Arrays.copyOfRange(copyFrom, 2, 9);        
+        for (String coffee : copyTo) {
+            System.out.print(coffee + " ");           
+        }            
+    }
+}
