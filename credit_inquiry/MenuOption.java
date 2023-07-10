6e5aa88d711a508cf64eb5bf1d11aace86f80717
miershooptier0 <113483260+miershooptier0@users.noreply.github.com>
2023-07-10 17:18:48 -0300

    Create MenuOption.java

---
+++ credit_inquiry/MenuOption.java
@@ -0,0 +1,12 @@
+package correspondencia_de_arquivos;
+
+public enum MenuOption {
+	
+	ZERO_BALANCE(0),
+	CREDIT_BALANCE(1),
+	DEBIT_BALANCE(2),
+	END(4);
+	
+		
+	private MenuOption(int value) {}
+}
