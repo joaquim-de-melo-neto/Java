db4365d90325b703b72a674aba3894d035c9e073
miershooptier0 <113483260+miershooptier0@users.noreply.github.com>
2023-07-13 18:29:20 -0300

    Create TimeComparator.java

---
+++ cap 16 - Coleções Genéricas/TimeComparator.java
@@ -0,0 +1,21 @@
+import java.util.Comparator;
+
+ public class TimeComparator implements Comparator<Time2>
+ {
+   @Override
+   public int compare(Time2 time1, Time2 time2)
+   {
+      int hourDifference = time1.getHour() - time2.getHour();
+  
+      if (hourDifference != 0) // testa a primeira hora
+        return hourCompare;
+  
+      int minuteDifference = time1.getMinute() - time2.getMinute();
+  
+      if (minuteDifference != 0) // então testa o minuto
+        return minuteDifference;
+  
+      int secondDifference = time1.getSecond() - time2.getSecond();
+        return secondDifference;
+  }
+} // fim da classe TimeComparator
