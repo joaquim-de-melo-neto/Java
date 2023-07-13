186b89f0b281f4662da13272f2400e4d10ebbda3
miershooptier0 <113483260+miershooptier0@users.noreply.github.com>
2023-07-13 18:59:10 -0300

    Update TimeComparator.java

--- cap 16 - Coleções Genéricas/TimeComparator.java
+++ cap 16 - Coleções Genéricas/TimeComparator.java
@@ -19,3 +19,31 @@ import java.util.Comparator;
         return secondDifference;
   }
 } // fim da classe TimeComparator
+
+// Método sort de Collections com um objeto Comparator personalizado.
+ import java.util.List;
+ import java.util.ArrayList;
+ import java.util.Collections;
+
+ public class Sort3
+ {
+   public static void main(String[] args)
+   {
+     List<Time2> list = new ArrayList<>(); // cria List
+  
+     list.add(new Time2(6, 24, 34));
+     list.add(new Time2(18, 14, 58));
+     list.add(new Time2(6, 05, 34));
+     list.add(new Time2(12, 14, 58));
+     list.add(new Time2(6, 24, 22));
+  
+   // gera saída de elementos List
+     System.out.printf("Unsorted array elements:%n%s%n", list);
+  
+  // classifica em ordem utilizando um comparador 
+     Collections.sort(list, new TimeComparator());
+  
+   // gera saída de elementos List
+     System.out.printf("Sorted list elements:%n%s%n", list);
+   }
+ } // fim da classe Sort3
